6293eabe33c3642134f77ced83b7e088f0eb1b4f
lagane100 <lagane100@gmail.com>
2015-02-28 12:47:15 +0800

    login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-    $(".panel").css("min-height", $("#login").height());
-    $("#container").css("min-height", $(".panel").height());
   $('#loginBtn').on('click', function(){
     $.ajax({
       type: "POST",
@@ -18,4 +16,9 @@ $(document).ready(function(){
       });
     });
   });
+});
+
+$(document).ready(function(){
+  $(".panel").css("min-height", $("#login").height());
+  $("#container").css("min-height", $(".panel").height());
 });
