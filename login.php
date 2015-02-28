2555e6e1c5c4d8d30d70436f0a8869dfdeccd543
lagane100 <lagane100@gmail.com>
2015-02-28 13:16:59 +0800

    login.php

--- login.php
+++ login.php
@@ -2,16 +2,18 @@
   require 'connect.php';
   $id = mysql_real_escape_string($_POST["id"]);
   $passwd = mysql_real_escape_string($_POST["passwd"]);
+  $Correct = array("Mode"=>'SUCCESS');
+  $Wrong = array("Mode"=>'ERROR');
   $result = mysql_query("SELECT account,password,UID FROM regist WHERE account = '$id' and password = '$passwd'");
   $row = mysql_fetch_array($result);
   if (count($row) != 1){
     setcookie("username", $id);
     setcookie("uid", $row["UID"]);
-    echo 'Success!';
+    echo JSON_encode($Correct);
     header("Location: login.html");
   }
   else{
-    echo 'Error!';
+    echo JSON_encode($Correct);
     header("Location: login.html");
   }
   mysql_close($con);
