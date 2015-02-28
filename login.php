a3c7a894ed2ab6638486b91ffc3c19f00e131664
lagane100 <lagane100@gmail.com>
2015-02-28 14:31:36 +0800

    login

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
   require 'connect.php';
-  $id = 123;//mysql_real_escape_string($_POST["id"]);
-  $passwd = 123;//mysql_real_escape_string($_POST["passwd"]);
+  $id = mysql_real_escape_string($_POST["id"]);
+  $passwd = mysql_real_escape_string($_POST["passwd"]);
   $Correct = array("Mode"=>'SUCCESS');
   $Wrong = array("Mode"=>'ERROR');
   $result = mysql_query("SELECT account,password,UID FROM regist WHERE account = '$id' and password = '$passwd'");
--- regist.php
+++ regist.php
@@ -4,17 +4,18 @@
   $passwd = mysql_real_escape_string($_POST["passwd"]);
   $sex = mysql_real_escape_string($_POST["sex"]);
   $racial = mysql_real_escape_string($_POST["racial"]);
-
+  $Correct = array("Mode"=>'SUCCESS');
+  $Wrong = array("Mode"=>'ERROR');
   $username = mysql_query("SELECT account FROM regist where account = '$id'");
   $row = mysql_fetch_array($username);
   if (count($row) == 1) {
     mysql_query("INSERT INTO regist (sex, account, password, racial) VALUES ('$sex', '$id', '$passwd', '$racial')");
     $UID = mysql_query("SELECT UID FROM regist where account = '$id'");
     mysql_query("INSERT INTO personal (UID, health, sex, racial, ATK, LV, killer, death, exp) VALUES ('$UID', 100, '$sex', '$racial', 10, 1, 0, 0, 0)");
-    echo 'Success';
+    echo JSON_encode($Correct["Mode"]);
   }
   else{
-    echo 'Error';
+    echo JSON_encode($Wrong["Mode"]);
   }
   mysql_close($con);
 ?>
