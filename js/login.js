bb2bf7f8eb35827d59333253730ce2614e9af124
lagane100 <lagane100@gmail.com>
2015-02-28 12:52:22 +0800

    login

--- js/login.js
+++ js/login.js
@@ -1,21 +1,6 @@
 $(document).ready(function(){
   $('#loginBtn').on('click', function(){
-    $.ajax({
-      type: "POST",
-      url: "login.php",
-      data: {
-        id: ('#Username')[0].value,
-        passwd: ('#Password')[0].value
-      },
-      dataType: 'json',
-      async: false,
-      success: function(data) {
-        alert("123");
-      }
-      error: function(data) {
-        alert("1234");
-      }
-    });
+    alert("123");
   });
 });
 
